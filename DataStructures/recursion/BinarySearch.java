64379c9aeb93321d7f51f9c481220d9b8253d1e7
sonakshi <sonakshi.katiyar@gmail.com>
2024-09-28 12:30:21 +0530

    Recursion basic using Binary Search

---
+++ DataStructures/recursion/BinarySearch.java
@@ -0,0 +1,32 @@
+package DataStructures.recursion;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 6, 8, 45, 51, 58, 62, 69, 90, 97};
+        search(arr, 97, 0, arr.length - 1);
+    }
+
+    /**
+     * start , end : should be figured out as arg variable, since either of
+     these would be changing
+     middle: middle's range is just till the function and if it is a match then this should be returned
+     */
+    public static int search(int[] arr, int element, int start, int end) {
+        if (start > end) { //base condition
+            System.out.println("Not found...!!");
+            return -1;
+        }
+        int middle = start + (end - start) / 2;
+        if (element == arr[middle]) {
+            System.out.println("at:" + middle);
+            return middle;
+        }
+        if (element > arr[middle]) {
+            start = middle + 1;
+            return search(arr, element, start, end);
+        }
+        end = middle - 1;
+        return search(arr, element, start, end);
+    }
+}
