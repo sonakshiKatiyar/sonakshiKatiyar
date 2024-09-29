61fc213fcd111d663caf15e53aa5a81ef563df0b
sonakshi <sonakshi.katiyar@gmail.com>
2024-09-29 14:01:43 +0530

    Linear Recursion basic printing Numer N to 1 then 1 to N

---
+++ DataStructures/recursion/nTo1Ton.java
@@ -0,0 +1,19 @@
+package DataStructures.recursion;
+
+public class nTo1Ton {
+    /**
+     * Print numbers N to 1
+     * then 1 to N*/
+    public static void main(String[] args) {
+        printN(5);
+    }
+    static void printN(int value){
+        if (value==0){
+            return;
+        }
+        System.out.println(value);
+        printN(value-1); // if you put printN(value--) this will throw Stack overflow Error
+        System.out.println(value);
+
+    }
+}
