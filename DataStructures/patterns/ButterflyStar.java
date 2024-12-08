c30f4e5ec6ba9c1c50a85f4316d4f67492607b85
sonakshi <sonakshi.katiyar@gmail.com>
2024-12-08 12:19:15 +0530

    Butterfly of stars

---
+++ DataStructures/patterns/ButterflyStar.java
@@ -0,0 +1,39 @@
+package DataStructures.patterns;
+
+public class ButterflyStar {
+    public static void main(String[] args) {
+        butterflyPattern(6);
+    }
+    public static void butterflyPattern(int n){
+        for (int i=1; i<=n; i++){
+            //star
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            //spaces
+            for(int k=1;k<=((n-i)*2);k++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=(2*n-(i-1));j<=2*n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >0 ; i--) {
+            //star
+            for(int j=(2*n-(i-1));j<=2*n;j++){
+                System.out.print("*");
+            }
+            //spaces
+            for(int k=1;k<=((n-i)*2);k++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
