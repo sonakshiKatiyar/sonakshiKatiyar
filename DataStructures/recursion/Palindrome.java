c7290c7adc8384e01fdb7af314535d68f67a1ba6
sonakshi <sonakshi.katiyar@gmail.com>
2025-02-17 23:03:47 +0530

    checking Palindrome using Recursion.

--- DataStructures/recursion/Palindrome.java
+++ DataStructures/recursion/Palindrome.java
@@ -2,17 +2,17 @@ package DataStructures.recursion;
 
 public class Palindrome {
     public static void main(String[] args) {
-        System.out.println(palindrom("MADAM", 0));
+        System.out.println(isPalindrome("MADAM", 0));
     }
 
-    public static boolean palindrom(String str, int i) {
+    public static boolean isPalindrome(String str, int i) {
         int n = str.length();
         if (i >= n / 2) {
             return true;
         }
         if (str.charAt(i) != str.charAt(n - 1 - i))
             return false;
-        return palindrom(str, i + 1);
+        return isPalindrome(str, i + 1);
 
     }
 }
